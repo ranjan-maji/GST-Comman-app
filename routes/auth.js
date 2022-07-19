0ce607f1d4207be096a5d63a54345164cbedcab7
ranjan-maji <99948732+ranjan-maji@users.noreply.github.com>
2022-07-19 15:40:59 +0530

    loginfix

--- routes/auth.js
+++ routes/auth.js
@@ -25,7 +25,7 @@ router.post('/register', async (req, res) => {
 router.post('/login', async (req, res) => {
      try {
         
-        const user = await User.findOne({username: req.body.username });
+        const user = await User.findOne({email: req.body.email });
         !user && res.status(401).json('Wrong Credentials!')
 
         const hashPassword = CryptoJS.AES.decrypt(user.password, process.env.PASS_SEC);
